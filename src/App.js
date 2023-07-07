06c184385d6300f956f74477db89ee0992bec7da
Enis <enis.mema@gmail.com>
2023-07-07 15:16:08 +0200

    Add styling for Daily stats

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import FeatureOne from './components/FeatureOne';
+import DailyStats from './components/DailyStats';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <HeroSection />
+      <DailyStats />
       <FeatureOne />
     </div>
   );
