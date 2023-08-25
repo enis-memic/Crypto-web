6ef31d8d5d057ef23968e5922499a7633b00d9d2
Enis <enis.mema@gmail.com>
2023-08-25 11:50:20 +0200

    Fix bugs

--- src/components/FeatureOne.jsx
+++ src/components/FeatureOne.jsx
@@ -2,16 +2,7 @@ import React from 'react';
 import TokenCard from './TokenCard';
 
 export const FeatureOne = () => {
-  const tokens = [
-    '0x',
-    'bitcoin',
-    'ethereum',
-    '0xmonero',
-    '1doge',
-    '3air',
-    'solana',
-    'xrp-classic-2',
-  ];
+  const tokens = ['bitcoin', 'ethereum', '1doge', 'solana'];
 
   return (
     <div className="feature-container">
